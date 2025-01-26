801b14fce4670c78379740ffa54d873c99eb929d
Your NameSaroj <Sarojreus10@gmail.com>
2025-01-26 18:49:12 +0545

    ok

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const mongoose = require("mongoose");
 const UserRouter = require("./Routes/user/User.Routes");
 require("dotenv").config();
 require("./models/User.models");
-app.use(express.json()); //middleware for body , body meaning the data we sent in payload in postman
+app.use(express.json()); //middleware for body , body meaning the data we sent in payload in postman used 
+// express.ßjson() converts this string into a JavaScript object so you can easily work with it in your app.
+
+
+// middle wares are dealt first before they run the code below them 
+
 mongoose
   .connect(process.env.mongo_connect, {})
   .then(() => {
